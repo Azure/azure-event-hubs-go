320aa9d020b3ce5217dcb121dd4cf77649fcbe9a
David Justice <david@devigned.com>
2018-07-25 17:09:10 -0700

    remove the iter limit from sender

--- sender.go
+++ sender.go
@@ -124,7 +124,7 @@ func (s *sender) trySend(ctx context.Context, evt eventer) error {
 	msg := evt.toMsg()
 	sp.SetTag("eh.message-id", msg.Properties.MessageID)
 
-	for i := 0; i < 10; i++ {
+	for {
 		// try as long as the context is not dead
 		err = s.sender.Send(ctx, msg)
 		if err == nil {
@@ -156,7 +156,6 @@ func (s *sender) trySend(ctx context.Context, evt eventer) error {
 			}
 		}
 	}
-	return err
 }
 
 func (s *sender) recover(ctx context.Context, err error) error {
