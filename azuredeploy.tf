15fb1fa42b5a0f89b95879b4f1a83b1014e22f6f
Jie Shen <drfish.me@gmail.com>
2019-09-18 10:01:07 +0800

    Remove condition check for role assignment in Terraform

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -101,18 +101,16 @@ resource "azuread_service_principal_password" "test" {
 
 # This provides the new AAD application the rights to managed, send and receive from the Event Hubs instance
 resource "azurerm_role_assignment" "service_principal_eh" {
-  count                = data.azurerm_client_config.current.service_principal_application_id == "" ? 1 : 0
   scope                = "subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.test.name}/providers/Microsoft.EventHub/namespaces/${azurerm_eventhub_namespace.test.name}"
   role_definition_name = "Azure Event Hubs Data Owner"
-  principal_id         = azuread_service_principal.test[0].id
+  principal_id         = data.azurerm_client_config.current.service_principal_application_id == "" ? azuread_service_principal.test[0].id : data.azurerm_client_config.current.service_principal_object_id
 }
 
 # This provides the new AAD application the rights to managed the resource group
 resource "azurerm_role_assignment" "service_principal_rg" {
-  count                = data.azurerm_client_config.current.service_principal_application_id == "" ? 1 : 0
   scope                = "subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.test.name}"
   role_definition_name = "Owner"
-  principal_id         = azuread_service_principal.test[0].id
+  principal_id         = data.azurerm_client_config.current.service_principal_application_id == "" ? azuread_service_principal.test[0].id : data.azurerm_client_config.current.service_principal_object_id
 }
 
 output "TEST_EVENTHUB_RESOURCE_GROUP" {
